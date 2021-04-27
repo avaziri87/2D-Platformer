6fcfc36b94a0c252e88a66c59e170aa9a3aa1779
LAPTOP-TESKCLAG\Alejandro <alejandrovaziri@gmail.com>
2021-04-27 13:45:32 -0400

    Added the walk animation to the charatcer

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -10,5 +10,9 @@ public class Player : MonoBehaviour
         var rigidbody2D = GetComponent<Rigidbody2D>();
 
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+
+        var animator = GetComponent<Animator>();
+        bool isWalking = horizontal != 0;
+        animator.SetBool("Walking", isWalking);
     }
 }
