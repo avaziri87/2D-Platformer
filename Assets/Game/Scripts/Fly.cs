4529449f2288f8097a575b2c135b7b1f2093c63e
LAPTOP-TESKCLAG\Alejandro <alejandrovaziri@gmail.com>
2021-04-27 16:11:58 -0400

    Direction and speed functionality to the Fly

--- Assets/Game/Scripts/Fly.cs
+++ Assets/Game/Scripts/Fly.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Fly : MonoBehaviour
 {
-    Vector3 _startPos;
-    Vector2 _direction = Vector2.up;
+    [SerializeField] Vector2 _direction = Vector2.up;
+    [SerializeField] float _maxDistance = 2;
+    [SerializeField] float _speed = 2;
+
+    Vector2 _startPos;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +17,13 @@ public class Fly : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(_direction * Time.deltaTime);
+        transform.Translate(_direction.normalized * _speed * Time.deltaTime);
 
         var distance = Vector2.Distance(_startPos, transform.position);
 
-        if(distance >= 2)
+        if(distance >= _maxDistance)
         {
+            transform.position = _startPos + (_direction.normalized * _maxDistance);
             _direction *= -1;
         }
     }
