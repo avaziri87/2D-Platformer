6d05873149b91a403eb8c7d1aee040c31f8e1d46
LAPTOP-TESKCLAG\Alejandro <alejandrovaziri@gmail.com>
2021-05-08 20:14:57 -0400

    Working Lava

---
+++ Assets/Game/Scripts/Key.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] KeyLock _keyLock;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            transform.SetParent(player.transform);
+            transform.localPosition = Vector3.up;
+        }
+
+        var keyLock = collision.GetComponent<KeyLock>();
+        if (keyLock != null && keyLock == _keyLock)
+        {
+            keyLock.Unlock();
+            Destroy(gameObject);
+        }
+    }
+}
